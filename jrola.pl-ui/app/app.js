ea310483026b48e1797187da5acdd491f9daefb4
jrola90 <jartsw@gmail.com>
2015-04-08 18:14:21 +0200

    new menu

--- jrola.pl-ui/app/app.js
+++ jrola.pl-ui/app/app.js
@@ -1,6 +1,14 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
+
+angular.module('myApp.index', []);
+angular.module('myApp.cv', []);
+angular.module('myApp.blog', []);
+angular.module('myApp.index', []);
+angular.module('myApp.contact', []);
+
+
 angular.module('myApp', [
     'ngRoute',
     'ui.bootstrap',
@@ -12,4 +20,26 @@ angular.module('myApp', [
 ]).
     config(['$routeProvider', function ($routeProvider) {
         $routeProvider.otherwise({redirectTo: '/index'});
+    }])
+    .controller('MyAppCtrl', ['$scope', function ($scope) {
+
+        $scope.state = {
+            index : true,
+            cv : false,
+            blog : false,
+            contact : false
+        };
+
+        $scope.selectMenuItem = function(key) {
+
+            // clear all states
+            for (var property in $scope.state) {
+                $scope.state[property] = false;
+            }
+
+            // set state2
+            $scope.state[key] = true;
+        };
+
+
     }]);
--- jrola.pl-ui/app/views/cv/cv.js
+++ jrola.pl-ui/app/views/cv/cv.js
@@ -10,4 +10,33 @@ angular.module('myApp.cv', ['ngRoute',
 ])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.otherwise({redirectTo: '/cv/skills'});
+    }])
+    .controller('CvCtrl', ['$scope', function ($scope) {
+
+        alert('tworzenie kontrollera');
+
+        $scope.state2 = {
+            skills : true,
+            experience : false,
+            projects : false,
+            courses : false,
+            education : false,
+            other : false
+        };
+
+        $scope.selectMenuItem = function(key) {
+
+            // clear all states
+            for (var property in $scope.state2) {
+                $scope.state2[property] = false;
+            }
+
+            // set state2
+            $scope.state2[key] = true;
+
+            //alert(key + "->" + $scope.state2[key]);
+
+        };
+
+
     }]);
